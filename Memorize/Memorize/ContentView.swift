ac4f662f5bc6928fb626ba0f1723c341a66db6bb
Loyd Hook IV <loyd.hook@aeronetica.com>
2024-07-16 17:30:48 -0500

    Added foreach stuff

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.colorScheme) var colorScheme
+    let emojis: Array<String> = ["🤓", "😍", "😎", "🤯"]
+
     var body: some View {
         HStack {
-            CardView(isFaceUp: false)
-            CardView()
-            CardView()
-            CardView()
+            ForEach(emojis.indices, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
         }
         .foregroundColor(.orange)
         .padding()
@@ -26,14 +27,16 @@ struct ContentView: View {
 }
 
 struct CardView: View {
+    let content: String
     @State var isFaceUp = true //@State makes this variable a pointer to isFaceUp
+
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("🤓")
+                Text(content)
                     .font(.largeTitle)
             } else {
                 base.fill()
@@ -41,7 +44,7 @@ struct CardView: View {
             }
         }
         .onTapGesture {
-            print("tappe")
+            print("tapped")
             isFaceUp.toggle()
         }
     }
