c58b7c1bdae5e21b05a219bdd357ceb5387d516d
Loyd Hook IV <loyd.hook@aeronetica.com>
2024-07-16 18:06:51 -0500

    Finished Required Task 1

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,16 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.colorScheme) var colorScheme
-    let emojis: Array<String> = ["🤓", "😍", "😎", "🤯"]
-
+    let emojis: Array<String> = ["🤓", "😍", "😎", "🤯", "😘", "🥶", "🥵", "🫡", "😱", "🫥"]
+    @State var cardCount = 4
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
+        VStack {
+            ScrollView{
+                cards
+            }
+            Spacer()
+            cardCountAdjuster
+        }
+        .padding()
+    }
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
                 CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-        .padding()
+    }
+    var cardCountAdjuster: some View {
+        HStack {
+            cardAdder
+            Spacer()
+            cardRemover
+        }
+        .font(.largeTitle)
+    }
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button{
+            cardCount += offset
+        } label: {
+            Image(systemName: symbol)
+        }
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus")
+    }
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus")
     }
 }
 
@@ -33,15 +65,13 @@ struct CardView: View {
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content)
                     .font(.largeTitle)
-            } else {
-                base.fill()
-                Text("")
             }
+            base.fill().opacity(isFaceUp ? 0: 1)
         }
         .onTapGesture {
             print("tapped")
